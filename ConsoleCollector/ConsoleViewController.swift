abe8e3baa527d918016592f7d391915d23c39d5e
Marc Joseph Streeter <marc.joseph.streeter@gmail.com>
2017-03-23 13:20:43 -0500

    I just finished lecture 74 section 7 proceeding to lecture 75  now we have a image an add button a textfield ,camera button, plus button , photo button

---
+++ ConsoleCollector/ConsoleViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ConsoleViewController.swift
+//  ConsoleCollector
+//
+//  Created by Marc Streeter on 3/23/17.
+//  Copyright © 2017 Developer Warriors. All rights reserved.
+//
+
+import UIKit
+
+class ConsoleViewController: UIViewController {
+
+   
+    
+    @IBOutlet weak var titleTextField: UITextField!
+   
+    @IBOutlet weak var consoleImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+  
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+    
+}
